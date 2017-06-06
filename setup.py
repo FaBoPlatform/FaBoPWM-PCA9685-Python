0f65e545e0fa301e7897e7630537f9942eae1930
Yoshiroh Takanashi <takanashi@gclue.jp>
2017-06-06 17:05:21 +0900

    new

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except:
+    long_description = ''
+
+classifiers = ['Development Status :: 4 - Beta',
+               'Operating System :: POSIX :: Linux',
+               'License :: OSI Approved :: Apache Software License',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 2.7',
+               'Topic :: Software Development',
+               'Topic :: System :: Hardware']
+
+setup(
+    name         = 'Fabo_PCA9685',
+    version      = '0.1',
+    author       = 'FaBo',
+    author_email = 'info@fabo.io',
+    description  = "This is a library for the PCA9685.",
+    long_description=long_description,
+    url          = 'https://github.com/FaBoPlatform/FaBoPWM-PCA9685-Python',
+    license      = 'Apache License 2.0',
+    classifiers  = classifiers,
+    packages     = find_packages()
+)
